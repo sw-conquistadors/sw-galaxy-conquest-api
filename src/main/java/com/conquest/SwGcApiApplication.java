c102ec08e78c02699266fa1e0f30c201abfddf22
Kenny Phu <kphu@outlook.com>
2022-01-13 10:19:42 -0500

    first commit

---
+++ src/main/java/com/conquest/SwGcApiApplication.java
@@ -0,0 +1,13 @@
+package com.conquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwGcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwGcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conquest/SwGcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwGcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
