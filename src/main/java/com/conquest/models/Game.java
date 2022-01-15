f096174cbb647a729595d9088707ae1531e5e6f5
Jesse Rodriguez <robotsarecrazy64@gmail.com>
2022-01-15 15:02:04 -0700

    Add Model Classes For Spring Application

---
+++ src/main/java/com/conquest/models/Game.java
@@ -0,0 +1,12 @@
+package com.conquest.models;
+
+import javax.persistence.Entity;
+
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Game {
+	// game has a SINGLE galaxy
+	// possibly some sort of scoring (User)
+}
--- src/main/java/com/conquest/models/Planet.java
+++ src/main/java/com/conquest/models/Planet.java
@@ -1,5 +1,14 @@
 package com.conquest.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 //Planets are the players primary challenge in the game as well as the source of resources
 //Planets may be made into factory worlds or recruiting worlds; each providing its own benefits
 //Factory worlds improve the quality of the players army allowing it to deal more damage and take less
@@ -9,7 +18,18 @@ package com.conquest.models;
 //Planets have various factors which determine how well they work as a factory, clone, or logistic world
 
 //Each game contains 20 planets randomly determined by the API
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="planets")
 public class Planet {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;// planet id 
+	private String url; // the planet's url
+	private String name;
+	private int type;
+	
 	//The diameter of a planet determines how well it serves as a factory
 	private int diameter;
 	
@@ -18,5 +38,18 @@ public class Planet {
 	
 	//The scope of a planet determines how many 
 	private int scope;
+	
+	private String climate;
+
+	public Planet(String url, String name, int type, int diameter, int population, int scope, String climate) {
+		super();
+		this.url = url;
+		this.name = name;
+		this.type = type;
+		this.diameter = diameter;
+		this.population = population;
+		this.scope = scope;
+		this.climate = climate;
+	}
 }
 
